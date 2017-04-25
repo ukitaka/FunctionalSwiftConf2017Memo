f7b13daa07083b4641ac880f23d2dfb9ccf81b75
ukitaka <yuki.takahashi.1126@gmail.com>
2017-04-25 11:21:42 +0900

    Add gist link

--- FunctionalSwiftConf2017.playground/Pages/A Type System From Scratch - Robert Widmann.xcplaygroundpage/Contents.swift
+++ FunctionalSwiftConf2017.playground/Pages/A Type System From Scratch - Robert Widmann.xcplaygroundpage/Contents.swift
@@ -1,4 +1,4 @@
 //: A Type System From Scratch - Robert Widmann
 //: - [Youtube](https://youtu.be/IbjoA5xVUq0)
 //: - [Presentations](https://github.com/CodaFi/Presentations)
-
+//: - [tinyurl.com/SwiftInfer](https://gist.github.com/CodaFi/ca35a0c22fbd96eca505b5df45f2509e)
