951143114a32c2f6197b3ec27222f96e42f5c3e1
ukitaka <yuki.takahashi.1126@gmail.com>
2017-04-26 11:23:20 +0900

    Add `Beyond Types in Swift – Brandon Kase`

---
+++ FunctionalSwiftConf2017.playground/Pages/Beyond Types in Swift – Brandon Kase.xcplaygroundpage/Contents.swift
@@ -0,0 +1,4 @@
+//: Beyond Types in Swift – Brandon Kase
+//: A Type System From Scratch - Robert Widmann
+//: - [Youtube](https://www.youtube.com/watch?v=6z9QjDUKkCs)
+//: - [Slide](https://github.com/bkase/slides/tree/master/beyond-type-safety-swift)
